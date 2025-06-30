834cd064c1ab66336b99ef1f6cc923be92a26ac7
LucaCiani <lucaciani88@gmail.com>
2025-06-30 17:33:12 +0200

    Add posts data and router for handling blog posts

--- routers/posts.js
+++ routers/posts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const posts = require("../data/posts");
+const posts = require("../data/postsData");
 
 router.get("/", (req, res) => {
     res.json(posts);
